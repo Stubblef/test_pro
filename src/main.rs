f994dab3d03ed51d32020eccb254feb40d2049a1
Stubblef <tsing967@gmail.com>
2024-03-05 11:31:53 +0800

    add
    Signed-off-by: Stubblef <tsing967@gmail.com>

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+// fn main() {
+//     println!("Hello, world!");
+// }
+
+use std::io;
+
+fn main() {
+    // 你的程序代码
+
+    // 在这里添加等待输入的语句
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+}
